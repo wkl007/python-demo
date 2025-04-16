558f26bcaab8f7576cecd61b5119ae1b557b5874
wkl007 <499657357@qq.com>
2025-04-16 11:20:53 +0800

    feat: chapter 2 调用函数

---
+++ chapter_2/1.py
@@ -0,0 +1,20 @@
+# abs 绝对值函数
+print(abs(-100), abs(2))
+
+# max 返回最大值
+print(max(1, 3, 2, 100))
+
+# int 转换为整数
+print(int('123'), 12.34)
+
+# float 转换为浮点数
+print(float(1), float('1'), float(12))
+
+# str 转换为字符串
+print(str(123))
+
+# bool 转换为 bool
+print(bool(0), bool(1))
+
+# hex 整数转十六进制
+print(hex(255), hex(1000))
