40497cccc98ff5822ac940d3c972448cf1a5ebfb
wkl007 <499657357@qq.com>
2025-04-17 23:31:31 +0800

    feat: chapter 10 urllib

---
+++ chapter_10/11.py
@@ -0,0 +1,69 @@
+import json
+from urllib import request, parse
+
+with request.urlopen('https://www.baidu.com') as f:
+    data = f.read()
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    # print('Data:', data.decode('utf-8'))
+
+print('---------------------------------')
+
+req = request.Request('http://www.douban.com/')
+req.add_header('User-Agent',
+               'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+with request.urlopen(req) as f:
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    # print('Data:', f.read().decode('utf-8'))
+
+# print('Login to weibo.cn...')
+# email = input('Email: ')
+# passwd = input('Password: ')
+# login_data = parse.urlencode([
+#     ('username', email),
+#     ('password', passwd),
+#     ('entry', 'mweibo'),
+#     ('client_id', ''),
+#     ('savestate', '1'),
+#     ('ec', ''),
+#     ('pagerefer', 'https://passport.weibo.cn/signin/welcome?entry=mweibo&r=http%3A%2F%2Fm.weibo.cn%2F')
+# ])
+#
+# req = request.Request('https://passport.weibo.cn/sso/login')
+# req.add_header('Origin', 'https://passport.weibo.cn')
+# req.add_header('User-Agent',
+#                'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+# req.add_header('Referer',
+#                'https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=http%3A%2F%2Fm.weibo.cn%2F')
+#
+# with request.urlopen(req, data=login_data.encode('utf-8')) as f:
+#     print('Status:', f.status, f.reason)
+#     for k, v in f.getheaders():
+#         print('%s: %s' % (k, v))
+#     print('Data:', f.read().decode('utf-8'))
+
+# proxy_handler = request.ProxyHandler({'http': 'http://www.example.com:3128/'})
+# proxy_auth_handler = request.ProxyBasicAuthHandler()
+# proxy_auth_handler.add_password('realm', 'host', 'username', 'password')
+# opener = request.build_opener(proxy_handler, proxy_auth_handler)
+# with opener.open('http://www.example.com/login.html') as f:
+#     pass
+
+
+
+
+def fetch_data(url):
+    with request.urlopen(url) as response:
+        data = response.read().decode('utf-8')
+        return json.loads(data)
+
+
+# 测试
+URL = 'https://api.weatherapi.com/v1/current.json?key=b4e8f86b44654e6b86885330242207&q=Beijing&aqi=no'
+data = fetch_data(URL)
+print(data)
+assert data['location']['name'] == 'Beijing'
+print('ok')
