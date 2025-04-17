80ffb8fa0210a149bded9d510b338350323c6a38
wkl007 <499657357@qq.com>
2025-04-17 22:46:03 +0800

    feat: chapter 10 argparse 命令行参数

---
+++ chapter_10/4.py
@@ -0,0 +1,42 @@
+# argparse 命令行参数
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='backup',  # 程序名
+        description='Backup MySQL database.',  # 描述
+        epilog='Copyright(c)，2025'  # 说明信息
+    )
+    # 定义位置参数:
+    parser.add_argument('outfile')
+    # 定义关键字参数:
+    parser.add_argument('--host', default='localhost')
+    # 此参数必须为int类型:
+    parser.add_argument('--port', default='3306', type=int)
+    # 允许用户输入简写的-u:
+    parser.add_argument('-u', '--user', required=True)
+    parser.add_argument('-p', '--password', required=True)
+    parser.add_argument('--database', required=True)
+    # gz参数不跟参数值，因此指定action='store_true'，意思是出现-gz表示True:
+    parser.add_argument('-gz', '--gzcompress', action='store_true', required=False, help='Compress backup files by gz.')
+
+    # 解析参数：
+    args = parser.parse_args()
+
+    # 打印参数:
+    print('parsed args:')
+    print(f'outfile = {args.outfile}')
+    print(f'host = {args.host}')
+    print(f'port = {args.port}')
+    print(f'user = {args.user}')
+    print(f'password = {args.password}')
+    print(f'database = {args.database}')
+    print(f'gzcompress = {args.gzcompress}')
+
+
+if __name__ == '__main__':
+    main()
+
+# python3 ./4.py -u root -p hello --database testdb backup.sql
