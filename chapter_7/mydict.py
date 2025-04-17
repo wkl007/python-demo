908974a226607d0bf916111aba8f51a632ec13e2
wkl007 <499657357@qq.com>
2025-04-17 14:37:23 +0800

    fix: chapter 7 文档测试

--- chapter_7/mydict.py
+++ chapter_7/mydict.py
@@ -1,4 +1,27 @@
 class Dict(dict):
+    """
+    Simple dict but also support access as x.y style.
+
+    >>> d1 = Dict()
+    >>> d1['x'] = 100
+    >>> d1.x
+    100
+    >>> d1.y = 200
+    >>> d1['y']
+    200
+    >>> d2 = Dict(a=1, b=2, c='3')
+    >>> d2.c
+    '3'
+    >>> d2['empty'] # doctest: +IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+        ...
+    KeyError: 'empty'
+    >>> d2.empty # doctest: +IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'Dict' object has no attribute 'empty'
+    """
+
     def __init__(self, **kw):
         super().__init__(**kw)
 
@@ -10,3 +33,11 @@ class Dict(dict):
 
     def __setattr__(self, key, value):
         self[key] = value
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
+
+# doctest: +IGNORE_EXCEPTION_DETAIL 让 doctest 不强制匹配完整异常，只匹配类型（避免干扰）。
