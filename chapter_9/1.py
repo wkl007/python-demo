48c0d018e585f2a41aa1be513fa5aeaf61a461c1
wkl007 <499657357@qq.com>
2025-04-17 16:00:06 +0800

    fix: chapter 9 多进程

---
+++ chapter_9/1.py
@@ -0,0 +1,98 @@
+import os
+import random
+import subprocess
+import time
+from multiprocessing import Process, Pool, Manager
+from queue import Queue
+
+
+# print('Process (%s) start...' % os.getpid())
+# # Only works on Unix/Linux/macOS:
+# pid = os.fork()
+# if pid == 0:
+#     print('I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
+# else:
+#     print('I (%s) just created a child process (%s).' % (os.getpid(), pid))
+
+
+def run_proc(name):
+    print('Run child process %s (%s)...' % (name, os.getpid()))
+
+
+# if __name__ == '__main__':
+#     print('Parent process %s.' % os.getpid())
+#     p = Process(target=run_proc, args=('test',))
+#     print('Child process will start.')
+#     p.start()
+#     p.join()
+#     print('Child process end.')
+
+def long_time_task(name):
+    print('Run task %s (%s)...' % (name, os.getpid()))
+    start = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+
+
+# if __name__ == '__main__':
+#     print('Parent process %s.' % os.getpid())
+#     p = Pool(4)
+#     for i in range(5):
+#         p.apply_async(long_time_task, args=(i,))
+#     print('Waiting for all subprocesses done...')
+#     p.close()
+#     p.join()
+#     print('All subprocesses done.')
+
+# 对Pool对象调用join()方法会等待所有子进程执行完毕，调用join()之前必须先调用close()，调用close()之后就不能继续添加新的Process了。
+
+
+# print('$ nslookup www.python.org')
+# r = subprocess.call(['nslookup', 'www.python.org'])
+# print('Exit code:', r)
+#
+# print('$ nslookup')
+# p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# output, err = p.communicate(b'set q=mx\npython.org\nexit\n')
+# print(output.decode('utf-8'))
+# print('Exit code:', p.returncode)
+
+# 写数据进程执行的代码:
+def write(q):
+    print('Process to write: %s' % os.getpid())
+    for value in ['A', 'B', 'C']:
+        print('Put %s to queue...' % value)
+        q.put(value)
+        time.sleep(random.random())
+
+
+# 读数据进程执行的代码:
+def read(q):
+    print('Process to read: %s' % os.getpid())
+    while True:
+        value = q.get(True)
+        print('Get %s from queue.' % value)
+
+
+if __name__ == '__main__':
+    from multiprocessing import freeze_support
+
+    freeze_support()  # Windows 和 macOS 上必要（特别是打包后）
+
+    with Manager() as manager:
+        q = manager.Queue()  # ✅ 用 Manager 创建 Queue
+        pw = Process(target=write, args=(q,))
+        pr = Process(target=read, args=(q,))
+        # 启动子进程pw，写入:
+        pw.start()
+        # 启动子进程pr，读取:
+        pr.start()
+        # 等待pw结束:
+        pw.join()
+        # pr进程里是死循环，无法等待其结束，只能强行终止:
+        pr.terminate()
+
+# 在Unix/Linux下，可以使用fork()调用实现多进程。
+# 要实现跨平台的多进程，可以使用multiprocessing模块。
+# 进程间通信是通过Queue、Pipes等实现的。
